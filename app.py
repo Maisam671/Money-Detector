722e79df3b186124385a6be10f3f9963c584179a
maisam aloubaidi <maisamaloubaidi@lau.edu>
2025-08-18 18:04:40 +0300

    flask

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def static_files(filename):
     return send_from_directory('static', filename)
 
 if __name__ == "__main__":
-    # Listen on all network interfaces, port 5000
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
