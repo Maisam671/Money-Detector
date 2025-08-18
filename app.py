0c9b9ad4f5927a58caa80571d117096f2204a034
Maisam671 <141740027+Maisam671@users.noreply.github.com>
2025-08-18 23:51:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,3 @@ def index():
 def static_files(filename):
     return send_from_directory('static', filename)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
